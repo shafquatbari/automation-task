94f46131c06b71c31de717921c35349247543727
shafquatulbari <shafquat.bari@brainstation-23.com>
2024-11-08 16:36:36 +0600

    uncomment first test case

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,41 +2,41 @@ import { expect } from "@wdio/globals";
 import fs from "fs";
 import csvParser from "csv-parser";
 
-// // Scenario 1: Verify Locked Out User
-// describe("Verify Locked Out User", () => {
-//   it("should display an error message for a locked-out user", async () => {
-//     // Activate the app
-//     await driver.activateApp("com.swaglabsmobileapp");
+// Scenario 1: Verify Locked Out User
+describe("Verify Locked Out User", () => {
+  it("should display an error message for a locked-out user", async () => {
+    // Activate the app
+    await driver.activateApp("com.swaglabsmobileapp");
 
-//     // Scroll to the element if needed
-//     await driver.execute("mobile: scroll", {
-//       strategy: "accessibility id",
-//       selector: "test-standard_user",
-//     });
+    // Scroll to the element if needed
+    await driver.execute("mobile: scroll", {
+      strategy: "accessibility id",
+      selector: "test-standard_user",
+    });
 
-//     // Click the selector to auto-fill the fields
-//     const autoFillSelector = await $(
-//       'android=new UiSelector().text("locked_out_user")'
-//     );
-//     await autoFillSelector.click();
+    // Click the selector to auto-fill the fields
+    const autoFillSelector = await $(
+      'android=new UiSelector().text("locked_out_user")'
+    );
+    await autoFillSelector.click();
 
-//     // Scroll to the element if needed
-//     await driver.execute("mobile: scroll", {
-//       strategy: "accessibility id",
-//       selector: "test-Username",
-//     });
+    // Scroll to the element if needed
+    await driver.execute("mobile: scroll", {
+      strategy: "accessibility id",
+      selector: "test-Username",
+    });
 
-//     // Click the login button
-//     const loginButton = await $("~test-LOGIN");
-//     await loginButton.click();
+    // Click the login button
+    const loginButton = await $("~test-LOGIN");
+    await loginButton.click();
 
-//     // Assert the locked-out error message
-//     const errorMessage = await $(
-//       'android=new UiSelector().text("Sorry, this user has been locked out.")'
-//     );
-//     await expect(errorMessage).toBeDisplayed();
-//   });
-// });
+    // Assert the locked-out error message
+    const errorMessage = await $(
+      'android=new UiSelector().text("Sorry, this user has been locked out.")'
+    );
+    await expect(errorMessage).toBeDisplayed();
+  });
+});
 
 // Scenario 2: Sign in as Standard User
 describe("Sign in as Standard User", () => {
